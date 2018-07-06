cddefc609baa97d261e0d6e8305752a59fdc4c64
TanninOne <sherb@gmx.net>
2018-07-06 18:24:36 +0200

    initial commit

---
+++ src/ChangelogDashlet.tsx
@@ -0,0 +1,99 @@
+import * as Promise from 'bluebird';
+import { IncomingMessage } from 'http';
+import { get } from 'https';
+import { IIssue } from 'nexus-api';
+import * as React from 'react';
+import { Button, Carousel, Pager } from 'react-bootstrap';
+import * as ReactDOM from 'react-dom';
+import { translate } from 'react-i18next';
+import { connect } from 'react-redux';
+import * as Redux from 'redux';
+import * as url from 'url';
+import { ComponentEx, Dashlet, log, Spinner, tooltip, types } from 'vortex-api';
+import * as va from 'vortex-api';
+
+const { EmptyPlaceholder } = va as any;
+
+interface IConnectedProps {
+  changelogs: Array<{ version: string, text: string, prerelease: boolean }>;
+}
+
+interface IActionProps {
+}
+
+type IProps = IConnectedProps & IActionProps;
+
+interface IIssueListState {
+  current: number;
+}
+
+class ChangelogDashlet extends ComponentEx<IProps, IIssueListState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.initState({
+      current: 0,
+    });
+  }
+
+  public render(): JSX.Element {
+    const { t, changelogs } = this.props;
+    const { current } = this.state;
+    return (
+      <Dashlet className='dashlet-changelog' title={t('Changelog')}>
+        {(current < changelogs.length) ? this.renderContent() : null}
+      </Dashlet>
+    );
+  }
+
+  private renderContent() {
+    const { t, changelogs } = this.props;
+    const { current } = this.state;
+
+    const changelog = changelogs[current];
+
+    return [
+       (
+        <Pager key={0}>
+          <Pager.Item previous disabled={current === 0} onClick={this.prev}>
+            {t('Previous')}
+          </Pager.Item>
+          {changelog.version}{changelog.prerelease ? ` (${t('Pre-release')})` : ''}
+          <Pager.Item next disabled={current === changelogs.length - 1} onClick={this.next}>
+            {t('Next')}
+          </Pager.Item>
+       </Pager>
+       ),
+      (
+        <div className='changelog-text' key={1}>
+          {changelog.text}
+        </div>
+       ),
+    ];
+  }
+
+  private prev = () => {
+    this.nextState.current = Math.max(0, this.state.current - 1);
+  }
+
+  private next = () => {
+    const { changelogs } = this.props;
+    this.nextState.current = Math.min(changelogs.length - 1, this.state.current + 1);
+  }
+}
+
+function mapStateToProps(state: any): IConnectedProps {
+  return {
+    changelogs: state.session.changelogs.changelogs,
+  };
+}
+
+function mapDispatchToProps(dispatch: any): IActionProps {
+  return {
+  };
+}
+
+export default
+  connect(mapStateToProps, mapDispatchToProps)(
+    translate(['changelog-dashlet', 'common'], { wait: true })(
+      ChangelogDashlet));
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import ChangelogDashlet from './ChangelogDashlet';
+import sessionReducer from './reducers';
+import { IGithubRelease } from './types';
+
+import * as Promise from 'bluebird';
+import { remote } from 'electron';
+import * as https from 'https';
+import * as _ from 'lodash';
+import * as path from 'path';
+import * as semver from 'semver';
+import * as url from 'url';
+import { types, util } from 'vortex-api';
+import { setChangelogs } from './actions';
+
+function updateReleases(store: Redux.Store<any>): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const relUrl = url.parse('https://api.github.com/repos/Nexus-Mods/Vortex/releases');
+    https.get({
+      ..._.pick(relUrl, ['port', 'hostname', 'path']),
+      headers: {
+        'User-Agent': 'Vortex',
+      },
+    }, res => {
+      res.setEncoding('utf-8');
+      let output = '';
+      res
+        .on('data', data => output += data)
+        .on('end', () => {
+          const appVersion = remote.app.getVersion();
+          const parsed: IGithubRelease[] = JSON.parse(output);
+          const current = parsed
+            .filter(rel =>
+              semver.valid(rel.name) && semver.gte(rel.name, appVersion))
+            .sort((lhs, rhs) => semver.compare(lhs.name, rhs.name));
+          store.dispatch(setChangelogs(
+            current.map(rel => ({
+              version: rel.name,
+              text: rel.body,
+              prerelease: rel.prerelease,
+             }))));
+        });
+    })
+    .on('error', err => {
+      reject(err);
+    })
+    .end();
+  });
+}
+
+function main(context: types.IExtensionContext) {
+  context.registerDashlet('Changelog', 1, 3, 200, ChangelogDashlet,
+    (state: types.IState) => true,
+  () => ({}), { closable: true });
+
+  context.registerReducer(['session', 'changelogs'], sessionReducer);
+
+  context.once(() => {
+    context.api.setStylesheet('changelog',
+      path.join(__dirname, 'changelog.scss'));
+    updateReleases(context.api.store);
+  });
+
+  return true;
+}
+
+export default main;
