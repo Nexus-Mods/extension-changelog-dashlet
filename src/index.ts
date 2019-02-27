fa9a562f0e24ba9dda00159b2be5f24da24b17d4
TanninOne <seppleviathan@gmx.de>
2019-02-27 13:35:41 +0100

    updated api and now using that for github access

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,33 @@
 import { setChangelogs } from './actions';
 import ChangelogDashlet from './ChangelogDashlet';
 import sessionReducer from './reducers';
-import { IGithubRelease } from './types';
 
 import * as Promise from 'bluebird';
-import * as https from 'https';
 import * as _ from 'lodash';
 import * as path from 'path';
 import * as Redux from 'redux';
-import * as semver from 'semver';
-import * as url from 'url';
 import { log, types, util } from 'vortex-api';
 
 function updateReleases(store: Redux.Store<any>): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    const relUrl = url.parse('https://api.github.com/repos/Nexus-Mods/Vortex/releases');
-    https.get({
-      ..._.pick(relUrl, ['port', 'hostname', 'path']),
-      headers: {
-        'User-Agent': 'Vortex',
-      },
-    }, res => {
-      res.setEncoding('utf-8');
-      const callsRemaining = parseInt(res.headers['x-ratelimit-remaining'] as string, 10);
-      if ((res.statusCode === 403) && (callsRemaining === 0)) {
-        const resetDate = parseInt(res.headers['x-ratelimit-reset'] as string, 10) * 1000;
-        log('info', 'GitHub rate limit exceeded',
-            { reset_at: (new Date(resetDate)).toString() });
-        return;
-      } else {
-        log('debug', 'remaining API calls', { count: callsRemaining });
+  return util.github.releases()
+    .then(releases => {
+      const state: types.IState = store.getState();
+      const persistentLogs =
+        util.getSafe(state, ['persistent', 'changelogs', 'changelogs'], []);
+
+      const len = releases.length;
+
+      if ((persistentLogs.length !== len)
+        || (persistentLogs[len - 1].version !== releases[len - 1].name)) {
+        const changeLogs = releases.map(rel => ({
+          version: rel.name,
+          text: rel.body,
+          prerelease: rel.prerelease,
+        }));
+
+        store.dispatch(setChangelogs(changeLogs));
       }
-      let output = '';
-      res
-        .on('data', data => output += data)
-        .on('end', () => {
-          try {
-            const parsed: IGithubRelease[] = JSON.parse(output);
-            if (!Array.isArray(parsed)) {
-              log('error', 'expected array of github releases', { got: parsed });
-              return;
-            }
-            // dropping releases before 0.12.7 because they weren't public and didn't have
-            // proper changelogs
-            const current = parsed
-              .filter(rel => semver.valid(rel.name) && semver.gte(rel.name, '0.12.7'))
-              .sort((lhs, rhs) => semver.compare(lhs.name, rhs.name));
-
-            const state: types.IState = store.getState();
-            const persistentLogs =
-              util.getSafe(state, ['persistent', 'changelogs', 'changelogs'], []);
-
-            const len = current.length;
-
-            if ((persistentLogs.length !== len)
-                || (persistentLogs[len - 1].name !== current[len - 1].name)) {
-              const changeLogs = current.map(rel => ({
-                version: rel.name,
-                text: rel.body,
-                prerelease: rel.prerelease,
-              }));
-
-              store.dispatch(setChangelogs(changeLogs));
-            }
-
-            resolve();
-          } catch (err) {
-            reject(err);
-          }
-        });
-    })
-    .on('error', err => {
-      reject(err);
-    })
-    .end();
-  });
+    });
 }
 
 function main(context: types.IExtensionContext) {
