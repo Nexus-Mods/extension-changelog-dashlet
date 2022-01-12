53c54ca2866710ee7b69434028bb0a6aa164fc0a
TanninOne <seppleviathan@gmx.de>
2022-01-12 15:40:05 +0100

    updated extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('changelog-dashlet', __dirname, 4);
+module.exports = webpack('changelog-dashlet', __dirname, 5);
 
